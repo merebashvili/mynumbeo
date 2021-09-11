0563ee0a4de005b8ec2927cd9305f11d3e70b757
Lasha Merebashvili <lashamerebashvili@protonmail.com>
2021-09-11 18:57:08 +0400

    Separate product model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,28 +5,3 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/mynumbeo');
 }
-
-const Product = mongoose.model('Product', {
-  "product": {
-    type: String,
-    required: true,
-    minLength: 2
-  },
-	"price_in_local": {
-    type: Number,
-    required: true,
-    min: 0.01
-  },
-	"price_in_usd": {
-    type: Number,
-    required: true,
-    min: 0.01
-  },
-  "quantity_for_month": {
-    type: Number,
-    required: true,
-    min: 1
-  }
-})
-
-product.save()
