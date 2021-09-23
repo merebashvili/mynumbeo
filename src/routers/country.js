12f31fa09251eb36541019fbd5ea22e56b49e461
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-09-23 18:06:59 +0400

    Add comment and remove unnecessary 'await'

--- src/routers/country.js
+++ src/routers/country.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Country = require('../models/country')
 
+// Get country data by country name along with its product ids
 router.get('/countries/:country', async (req, res) => {
     const countryName = req.params.country
 
     try {
         // Here we link the existing products to the country's 'products' property according to their _ids
-        await Country.findOne({name: countryName}).populate('products').exec(function (err, country) {
+        Country.findOne({name: countryName}).populate('products').exec(function (err, country) {
             if (err) return handleError(err);
             if (!country) {
                 return res.status(404).send()
