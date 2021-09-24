05698902d3508ec9f3d539efedad12573db71924
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-09-24 11:04:39 +0400

    Add country updating endpoint

--- src/routers/country.js
+++ src/routers/country.js
@@ -31,4 +31,39 @@ router.get('/countries/:country', async (req, res) => {
     }
 })
 
+router.patch('/countries/:id', async (req, res) => {
+    const _id = req.params.id
+
+    // TO DO
+    // See if there is a shorter way to validate
+    if (!isValidOperation(req.body)) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        /* Without {returnOriginal: false}, productToBeUpdated will give me the old (preupdated) product.
+        Also, without {runValidators: true}, there will be no validation check,
+        e.g. I can set "quantity_for_month" to 0, even though I have set the
+        minimum quantity to 1 in product schema (./models/product) */
+        const countryToBeUpdated = await Country.findByIdAndUpdate(_id, req.body, {returnOriginal: false, runValidators: true})
+
+        if (!countryToBeUpdated) {
+            return res.status(400).send()
+        }
+
+        res.send(countryToBeUpdated)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+/* Checks if the property we are trying to update is valid - meaning,
+if it is included inside the product schema (./models/product) I created earlier */
+const isValidOperation = (body) => {
+    const updates = Object.keys(body)
+    const allowedUpdates = ['name']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    return isValidOperation
+}
+
 module.exports = router
