4a94b041d84aada63b17874a9113e0900d572ad7
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-09-29 10:16:49 +0400

    Add user logout from all sessions

--- src/routers/user.js
+++ src/routers/user.js
@@ -29,6 +29,17 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
