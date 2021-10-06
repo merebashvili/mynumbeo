42ff4599afcd9d35f53bf245716ea1c9fe09f245
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-06 12:23:36 +0400

    Restructure in order to access (for testing) express app without calling app.listen()

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const productRouter = require('./routers/product')
+const countryRouter = require('./routers/country')
+const userRouter = require('./routers/user')
+
+//connect Mongoose to the db
+require('./db/mongoose')
+
+const app = express()
+
+// Automatically parse incoming JSON to an object
+app.use(express.json())
+app.use(productRouter)
+app.use(countryRouter)
+app.use(userRouter)
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-const express = require('express')
-const productRouter = require('./routers/product')
-const countryRouter = require('./routers/country')
-const userRouter = require('./routers/user')
-
-//connect Mongoose to the db
-require('./db/mongoose')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-// Automatically parse incoming JSON to an object
-app.use(express.json())
-app.use(productRouter)
-app.use(countryRouter)
-app.use(userRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
