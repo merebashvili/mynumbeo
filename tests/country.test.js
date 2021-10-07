2ea32c1aba8844adc5c9b521e2f1d8968ee80d82
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-07 12:08:33 +0400

    Create tests for getting user countries

--- tests/country.test.js
+++ tests/country.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../src/app');
 const Country = require('../src/models/country');
-const { setupDatabase } = require('./db');
+const { testUser, testUserId, setupDatabase } = require('./db');
 
 // making sure that every test that runs, runs in the same environment,
 // with the same test data in the database
@@ -9,4 +9,14 @@ beforeEach(async () => {
   await setupDatabase();
 });
 
-test('', () => {});
+test('Should get countries for user', async () => {
+  await request(app)
+    .get('/countries')
+    .set('Authorization', `Bearer ${testUser.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('Should NOT get countries for unauthenticated user', async () => {
+  await request(app).get('/countries').send().expect(401);
+});
