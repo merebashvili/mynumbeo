051381ef73bc719f268366275a170c2945ec5d8e
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-07 13:16:21 +0400

    Add test product & test country

--- tests/db.js
+++ tests/db.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../src/models/user');
+const Product = require('../src/models/product');
+const Country = require('../src/models/country');
 
 const testUserId = new mongoose.Types.ObjectId();
 const testUser = {
@@ -15,11 +17,35 @@ const testUser = {
   ],
 };
 
+const testCountryId = new mongoose.Types.ObjectId();
+const testProductId = new mongoose.Types.ObjectId();
+const testCountry = {
+  _id: testCountryId,
+  name: 'Georgia',
+  products: [testProductId],
+  owner: testUser._id,
+};
+
+const testProduct = {
+  _id: new mongoose.Types.ObjectId(),
+  product: 'testProductName',
+  price_in_local: 15,
+  price_in_usd: 5,
+  quantity_for_month: 2,
+  country: testCountry._id,
+  owner: testUser._id,
+};
+
 const setupDatabase = async () => {
-  //delete every user in the database
+  //delete every user, product & country in the database
   await User.deleteMany();
-  //start with one user for testing other endpoints like login
+  await Product.deleteMany();
+  await Country.deleteMany();
+  //start with one user, one task & one country for testing
+  // other endpoints like login, getting task, getting country, etc.
   await new User(testUser).save();
+  await new Product(testProduct).save();
+  await new Country(testCountry).save();
 };
 
 module.exports = {
